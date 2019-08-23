100f89c15a65274c96e9b0dc38fdaaa8007ba5b9
Nurudeen <nurudeen.amedu@gmail.com>
2019-08-23 10:40:43 +0100

    objects refactored to static object creation

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -14,14 +14,33 @@ class dinosaurs {
     this.period = period_;
   }
 }
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-let tyrannosaurus = new dinosaurs('tyrannosaurus','carnivorous','7000kg','12m','Late Cretaceous');
+let tyrannosaurus = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  roar: () => `RAWERSRARARWERSARARARRRR!`,
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-let stegosaurus = new dinosaurs('stegosaurus','herbivorous','2000kg','9m','Late Jurassic');
-
+let stegosaurus = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic',
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-let velociraptor = new dinosaurs('velociraptor','carnivorous','15kg','1.8m','Late Cretaceous');
+let velociraptor = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous',
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
@@ -38,10 +57,6 @@ console.log(stegosaurus.length);
 console.log(tyrannosaurus.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-tyrannosaurus.roar = () => {
-  return `RAWERSRARARWERSARARARRRR!`;
-}
-
 console.log(tyrannosaurus ,tyrannosaurus.roar());
 
 
